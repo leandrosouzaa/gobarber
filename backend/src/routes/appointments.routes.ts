0ab8cd89d9d2602ba3a99ab44c2c8c2d62efca93
Leandro Souza <leandror.dev@gmail.com>
2020-04-13 13:37:57 -0300

    Validação no horário de agendamento

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,17 +1,35 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+   id: string;
+   provider: string;
+   date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (req, res) => {
    const { provider, date } = req.body;
 
+   const parsedDate = startOfHour(parseISO(date));
+   const findAppointmentInSameDate = appointments.find(a =>
+      isEqual(parsedDate, a.date),
+   );
+
+   if (findAppointmentInSameDate) {
+      return res
+         .status(400)
+         .json({ message: 'This appointment is already booked' });
+   }
+
    const appointment = {
       id: uuid(),
       provider,
-      date,
+      date: parsedDate,
    };
 
    appointments.push(appointment);
