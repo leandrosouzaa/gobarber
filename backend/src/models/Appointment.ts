80768738983e15a28c40cdc7bf8b01a3ac335ad9
Leandro Souza <leandror.dev@gmail.com>
2020-04-13 15:40:29 -0300

    DTO

--- backend/src/models/Appointment.ts
+++ backend/src/models/Appointment.ts
@@ -1,5 +1,10 @@
 import { uuid } from 'uuidv4';
 
+interface AppointmentDTO {
+   provider: string;
+   date: Date;
+}
+
 class Appointment {
    id: string;
 
@@ -7,7 +12,7 @@ class Appointment {
 
    date: Date;
 
-   constructor(provider: string, date: Date) {
+   constructor({ provider, date }: Omit<Appointment, 'id'>) {
       this.id = uuid();
       this.provider = provider;
       this.date = date;
--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -27,7 +27,10 @@ appointmentsRouter.post('/', (req, res) => {
          .json({ message: 'This appointment is already booked' });
    }
 
-   const appointment = appointmentsRepository.create(provider, parsedDate);
+   const appointment = appointmentsRepository.create({
+      provider,
+      date: parsedDate,
+   });
 
    return res.json(appointment);
 });
