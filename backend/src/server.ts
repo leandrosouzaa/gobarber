73ff0a78f2749773f98595573ec8c775518ba349
Leandro Souza <leandror.dev@gmail.com>
2020-04-13 11:18:36 -0300

    Configuração ESlint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,9 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-   return res.json({message: 'Olá Mundo'});
-});
+app.get('/', (req, res) => res.json({ message: 'Olá Mundo' }));
 
 app.listen(3333, () => {
-   console.log('👷 Server started in port 3333!')
-})
+  console.log('👷 Server started in port 3333!');
+});
