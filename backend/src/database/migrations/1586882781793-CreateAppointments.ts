d14608b80b4af388d7e878aa4454b7c9495c3901
Leandro Souza <leandror.dev@gmail.com>
2020-04-14 14:12:43 -0300

    Model de Migration com TypeORM

--- backend/src/database/migrations/1586882781793-CreateAppointments.ts
+++ backend/src/database/migrations/1586882781793-CreateAppointments.ts
@@ -1,5 +1,4 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
-import { query } from 'express';
 
 export default class CreateAppointments1586882781793
    implements MigrationInterface {
