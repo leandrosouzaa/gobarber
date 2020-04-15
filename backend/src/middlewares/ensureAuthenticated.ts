00ca69dab19e361adba846196c4a725955f02f9f
Leandro Souza <leandror.dev@gmail.com>
2020-04-15 18:44:59 -0300

    Upload de arquivos com Multer

--- backend/src/middlewares/ensureAuthenticated.ts
+++ backend/src/middlewares/ensureAuthenticated.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/class-name-casing */
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
@@ -31,7 +32,6 @@ export default function ensureAuthenticated(
          id: sub,
       };
 
-      console.log(decoded);
       return next();
    } catch (err) {
       throw new Error('Invalid JWT token');
--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import CreateUserService from '../services/CreateUserService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+import uploadConfig from '../config/upload';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
    try {
@@ -18,5 +24,14 @@ usersRouter.post('/', async (req, res) => {
       return res.status(400).json({ message: err.message });
    }
 });
+
+usersRouter.patch(
+   '/avatar',
+   ensureAuthenticated,
+   upload.single('avatar'),
+   async (req, res) => {
+      return res.json({ ok: true });
+   },
+);
 
 export default usersRouter;
