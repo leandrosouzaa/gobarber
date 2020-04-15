4eeac7d724215879865b9ba8a00d58284e248374
Leandro Souza <leandror.dev@gmail.com>
2020-04-15 10:34:19 -0300

    Autenticação de Usuários

---
+++ backend/src/routes/sessions.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (req, res) => {
+   try {
+      const { email, password } = req.body;
+      const authenticateUser = new AuthenticateUserService();
+
+      const { user } = await authenticateUser.execute({ email, password });
+      delete user.password;
+
+      return res.json(user);
+   } catch (err) {
+      return res.status(400).json({ message: err.message });
+   }
+});
+
+export default sessionsRouter;
