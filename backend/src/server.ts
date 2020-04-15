b4a80dbcbc990e077f6aaf82de92dc85cb1759a3
Leandro Souza <leandror.dev@gmail.com>
2020-04-15 19:39:31 -0300

    Visualização de Imagens.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import 'reflect-metadata';
 
+import uploadConfig from './config/upload';
+
 import routes from './routes';
 import './database';
 
 const app = express();
 
 app.use(express.json());
-
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
