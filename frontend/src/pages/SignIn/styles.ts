0fe9e1911118aafad408aee7a46fd1d6a03c3245
Leandro Souza <leandror.dev@gmail.com>
2020-04-22 20:35:03 -0300

    SignIn styles

--- frontend/src/pages/SignIn/styles.ts
+++ frontend/src/pages/SignIn/styles.ts
@@ -1,7 +1,103 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-export const Container = styled.div``;
+import signInBackground from '../../assets/sign-in-background.png';
 
-export const Content = styled.div``;
+export const Container = styled.div`
+   height: 100vh;
 
-export const Background = styled.div``;
+   display: flex;
+   align-items: stretch;
+`;
+
+export const Content = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+
+   place-content: center;
+   width: 100%;
+   max-width: 700px;
+
+   form {
+      margin: 80px 0;
+      width: 340px;
+      text-align: center;
+      align-items: center;
+
+      h1 {
+         margin-bottom: 24px;
+      }
+
+      input {
+         background: #232129;
+         color: #f4ede8;
+         border-radius: 10px;
+         border: 2px solid #232129;
+         padding: 16px;
+         width: 100%;
+
+         & + input {
+            margin-top: 8px;
+         }
+
+         &::placeholder {
+            color: #666360;
+         }
+      }
+
+      button {
+         background: #ff9000;
+         border-radius: 10px;
+         border: 0;
+         height: 56px;
+         padding: 0 16px;
+         width: 100%;
+         color: #312e38;
+         font-weight: 500;
+         margin-top: 16px;
+         transition: background-color 0.2s;
+
+         &:hover {
+            background: ${shade(0.2, '#ff9000')};
+         }
+      }
+
+      a {
+         color: #f4ede8;
+         display: block;
+         margin-top: 24px;
+         text-decoration: none;
+         transition: color 0.2s;
+
+         &:hover {
+            color: ${shade(0.2, '#f4ede8')};
+         }
+      }
+   }
+
+   > a {
+      color: #ff9000;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      display: flex;
+      align-items: center;
+
+      svg {
+         margin-right: 16px;
+      }
+
+      &:hover {
+         color: ${shade(0.2, '#ff9000')};
+      }
+   }
+`;
+
+export const Background = styled.div`
+   flex: 1;
+   background: url(${signInBackground}) no-repeat;
+   background-size: cover;
+`;
