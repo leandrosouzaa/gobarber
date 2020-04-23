23313124784abe6748150e53ae1ea49843e8e365
Leandro Souza <leandror.dev@gmail.com>
2020-04-23 20:34:03 -0300

    Create useAuth hook

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,11 +1,11 @@
-import React, { useRef, useCallback, useContext } from 'react';
+import React, { useRef, useCallback } from 'react';
 
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
-import { AuthContext } from '../../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 import getValidationErros from '../../utils/getValidationErrors';
 import { Input, Button } from '../../components';
 import { Container, Content, Background } from './styles';
@@ -20,9 +20,7 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
    const formRef = useRef<FormHandles>(null);
 
-   const { signIn, user } = useContext(AuthContext);
-
-   console.log(user);
+   const { signIn } = useAuth();
 
    const handleSubmit = useCallback(
       async (data: SignInFormData): Promise<void> => {
