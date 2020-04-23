3da91fc89578c26343d1d84d0c29acba69c7bc3c
Leandro Souza <leandror.dev@gmail.com>
2020-04-23 19:17:25 -0300

    Auth context (!done)

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,10 +1,11 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
+import AuthContext from '../../context/AuthContext';
 import getValidationErros from '../../utils/getValidationErrors';
 import { Input, Button } from '../../components';
 import { Container, Content, Background } from './styles';
@@ -13,6 +14,9 @@ import logo from '../../assets/logo.svg';
 
 const SignIn: React.FC = () => {
    const formRef = useRef<FormHandles>(null);
+
+   const { name } = useContext(AuthContext);
+   console.log(name);
 
    const handleSubmit = useCallback(async (data: object): Promise<void> => {
       try {
