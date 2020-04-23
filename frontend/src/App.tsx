bef7d9f9370faffcdf31054409c0a74ce9a9e3b5
Leandro Souza <leandror.dev@gmail.com>
2020-04-23 20:09:11 -0300

    Change AuthContext

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
-import AuthContext from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
    <>
-      <AuthContext.Provider value={{ name: 'Leandro' }}>
+      <AuthProvider>
          <SignIn />
-      </AuthContext.Provider>
+      </AuthProvider>
       <GlobalStyle />
    </>
 );
