79fd9e5bb842c9fefbeaa5c8e3518fc366d7d389
Leandro Souza <leandror.dev@gmail.com>
2020-04-24 11:23:02 -0300

    create AppProvider component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
-import { AuthProvider } from './hooks/AuthContext';
-import { ToastContainer } from './components';
+import AppProvider from './hooks';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
    <>
-      <AuthProvider>
+      <AppProvider>
          <SignIn />
-      </AuthProvider>
-      <ToastContainer />
+      </AppProvider>
       <GlobalStyle />
    </>
 );
