55257b72e8103a766177d6cb20722abea7277c73
Leandro Souza <leandror.dev@gmail.com>
2020-04-27 17:57:40 -0300

    validate forms with Yup

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -7,16 +7,26 @@ import {
    Platform,
    ScrollView,
    TextInput,
+   Alert,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
 
+import getValidationErros from '../../utils/getValidationErros';
 import { Button, Input } from '../../components';
 import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 
 import logoImg from '../../assets/logo.png';
+
+interface SignUpFormData {
+   name: string;
+   email: string;
+   password: string;
+   confirmPassword: string;
+}
 
 const SignUp: React.FC = () => {
    const formRef = useRef<FormHandles>(null);
@@ -27,10 +37,48 @@ const SignUp: React.FC = () => {
 
    const navigation = useNavigation();
 
-   const handleSignUp = useCallback((data: object) => {
-      console.log(data);
-   }, []);
+   const handleSignUp = useCallback(async (data: SignUpFormData): Promise<
+      void
+   > => {
+      try {
+         formRef.current?.setErrors({});
 
+         const schema = Yup.object().shape({
+            name: Yup.string().required('Nome obrigatório'),
+            email: Yup.string()
+               .required('E-mail obrigatório')
+               .email('Digite um e-mail válido'),
+            password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+            confirmPassword: Yup.string().oneOf(
+               [Yup.ref('password'), null],
+               'As senhas não coincidem',
+            ),
+         });
+
+         await schema.validate(data, {
+            abortEarly: false,
+         });
+
+         // await api.post('/users', data);
+
+         Alert.alert(
+            'Cadastro realizado!',
+            'Vocẽ já pode fazer logon no GoBarber',
+         );
+      } catch (err) {
+         if (err instanceof Yup.ValidationError) {
+            const errors = getValidationErros(err);
+
+            formRef.current?.setErrors(errors);
+            return;
+         }
+
+         Alert.alert(
+            'Aconteceu um erro',
+            'Ocorreu um erro durante o cadastro, tente novamente',
+         );
+      }
+   }, []);
    return (
       <>
          <KeyboardAvoidingView
