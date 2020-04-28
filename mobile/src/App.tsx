4a53d1b0106e5a77bd240033a6e79ac679529c62
Leandro Souza <leandror.dev@gmail.com>
2020-04-27 23:10:57 -0300

    create auth context and use async storage

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -5,13 +5,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { View, StatusBar } from 'react-native';
 
 import Routes from './routes';
+import AppProvider from './hooks';
 
 const App: React.FC = () => (
    <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-      <View style={{ backgroundColor: '#312e38', flex: 1 }}>
-         <Routes />
-      </View>
+      <AppProvider>
+         <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+            <Routes />
+         </View>
+      </AppProvider>
    </NavigationContainer>
 );
 
