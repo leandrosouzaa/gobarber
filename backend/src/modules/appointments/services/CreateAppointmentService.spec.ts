03fc9ce836d4f26cffce2fe1dfd6266991fcd37f
leandrosouzaa <leandror.dev@gmail.com>
2020-05-06 10:08:51 -0300

    appointments tests

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentsService from './CreateAppointmentService';
 
@@ -17,7 +18,24 @@ describe('CreateAppointment', () => {
       expect(appointment.provider_id).toBe('123');
    });
 
-   // it('should not be able to create two appointments on the same time', () => {
-   //    expect(1 + 2).toBe(3);
-   // });
+   it('should not be able to create two appointments on the same time', async () => {
+      const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+      const createAppointment = new CreateAppointmentsService(
+         fakeAppointmentsRepository,
+      );
+
+      const appointmentDate = new Date(2020, 4, 10, 11);
+
+      await createAppointment.execute({
+         date: appointmentDate,
+         provider_id: '123',
+      });
+
+      expect(
+         createAppointment.execute({
+            date: appointmentDate,
+            provider_id: '123',
+         }),
+      ).rejects.toBeInstanceOf(AppError);
+   });
 });
