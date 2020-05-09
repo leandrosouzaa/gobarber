477b9d6c8daaeee245b2cfaf365cd9669c37cb02
leandrosouzaa <leandror.dev@gmail.com>
2020-05-09 13:05:38 -0300

    profile routes (update and show)

---
+++ backend/src/modules/users/infra/http/controllers/ProfileController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import UpdateProfileService from '@modules/users/services/UpdateProfileService';
+import ShowProfileService from '@modules/users/services/ShowProfileService';
+
+export default class UserAvatarController {
+   public async show(req: Request, res: Response): Promise<Response> {
+      const user_id = req.user.id;
+
+      const showProfile = container.resolve(ShowProfileService);
+
+      const user = await showProfile.execute({ user_id });
+
+      return res.json(user);
+   }
+
+   public async update(req: Request, res: Response): Promise<Response> {
+      const { name, email, old_password, password } = req.body;
+      const user_id = req.user.id;
+
+      const updateProfile = container.resolve(UpdateProfileService);
+
+      const user = await updateProfile.execute({
+         user_id,
+         name,
+         email,
+         old_password,
+         password,
+      });
+      delete user.password;
+
+      return res.json(user);
+   }
+}
---
+++ backend/src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,31 @@
+import { inject, injectable } from 'tsyringe';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+   user_id: string;
+}
+
+@injectable()
+class ShowProfileService {
+   constructor(
+      @inject('UsersRepository') private usersRepository: IUsersRepository,
+   ) {}
+
+   public async execute({ user_id }: IRequest): Promise<User> {
+      const user = await this.usersRepository.findById(user_id);
+
+      if (!user) {
+         throw new AppError('User not found');
+      }
+
+      delete user.password;
+
+      return user;
+   }
+}
+
+export default ShowProfileService;
