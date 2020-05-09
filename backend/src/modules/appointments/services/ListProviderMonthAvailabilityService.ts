f7941e452b695807c9dafffeecb23211c7d937bd
leandrosouzaa <leandror.dev@gmail.com>
2020-05-09 19:10:38 -0300

    ListProviderMonthavailability complete and tests

--- backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import { inject, injectable } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -34,9 +35,25 @@ class ListProviderAvailabilityService {
          },
       );
 
-      console.log(appointments);
+      const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
 
-      return [{ day: 1, available: false }];
+      const eachDayArray = Array.from(
+         { length: numberOfDaysInMonth },
+         (_, index) => index + 1,
+      );
+
+      const availability = eachDayArray.map(day => {
+         const appointmentsInDay = appointments.filter(appointment => {
+            return getDate(appointment.date) === day;
+         });
+
+         return {
+            day,
+            available: appointmentsInDay.length < 10,
+         };
+      });
+
+      return availability;
    }
 }
 
