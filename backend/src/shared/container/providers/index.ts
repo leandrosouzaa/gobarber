6e19f92c340f757988e9a593acb0dd4bc021e3bf
leandrosouzaa <leandror.dev@gmail.com>
2020-05-08 21:22:47 -0300

    register mailProvider dependency

--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
+import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherialMailProvider from './MailProvider/implementations/EtherialMailProvider';
+
 container.registerSingleton<IStorageProvider>(
    'StorageProvider',
    DiskStorageProvider,
+);
+
+container.registerInstance<IMailProvider>(
+   'MailProvider',
+   new EtherialMailProvider(),
 );
