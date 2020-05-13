a2e3bbfa3744b190670aa81fd8970f835c541f8b
leandrosouzaa <leandror.dev@gmail.com>
2020-05-13 11:29:33 -0300

    Notifications Repository

---
+++ backend/src/modules/notifications/repositories/INotificationsRepository.ts
@@ -0,0 +1,7 @@
+import Notification from '../infra/typeorm/schemas/Notification';
+
+import ICreateNotificationDTO from '../dtos/ICreateNotificationDTO';
+
+export default interface INotificationsRepository {
+   create(data: ICreateNotificationDTO): Promise<Notification>;
+}
