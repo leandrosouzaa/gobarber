e64b5368cd470f9830f2664b9e7e3a48cef2b930
leandrosouzaa <leandror.dev@gmail.com>
2020-05-14 13:51:44 -0300

    add cache feature

--- backend/src/modules/appointments/services/ListProvidersService.ts
+++ backend/src/modules/appointments/services/ListProvidersService.ts
@@ -1,7 +1,9 @@
 import { inject, injectable } from 'tsyringe';
 
 import User from '@modules/users/infra/typeorm/entities/User';
+
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
    user_id: string;
@@ -10,13 +12,26 @@ interface IRequest {
 @injectable()
 class ListProvidersService {
    constructor(
-      @inject('UsersRepository') private usersRepository: IUsersRepository,
+      @inject('UsersRepository')
+      private usersRepository: IUsersRepository,
+
+      @inject('CacheProvider')
+      private cacheProvider: ICacheProvider,
    ) {}
 
    public async execute({ user_id }: IRequest): Promise<User[]> {
-      const users = await this.usersRepository.findAllProviders({
-         except_user_id: user_id,
-      });
+      let users = await this.cacheProvider.recover<User[]>(
+         `providers-list:${user_id}`,
+      );
+
+      if (!users) {
+         users = await this.usersRepository.findAllProviders({
+            except_user_id: user_id,
+         });
+
+         console.log('A query no banco foi feita');
+         await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      }
 
       return users;
    }
