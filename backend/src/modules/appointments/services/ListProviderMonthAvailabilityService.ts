8dbbf2d8f967ecacb655695776a12c79cebb52c1
leandrosouzaa <leandror.dev@gmail.com>
2020-05-22 10:21:08 -0300

    fix in providerMonthAvailability

--- backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import { inject, injectable } from 'tsyringe';
-import { getDaysInMonth, getDate } from 'date-fns';
+import { getDaysInMonth, getDate, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -43,15 +43,21 @@ class ListProviderMonthAvailabilityService {
       );
 
       const availability = eachDayArray.map(day => {
+         const compareDate = new Date(year, month - 1, day, 23, 59, 59);
+
          const appointmentsInDay = appointments.filter(appointment => {
             return getDate(appointment.date) === day;
          });
 
          return {
             day,
-            available: appointmentsInDay.length < 10,
+            available:
+               isAfter(compareDate, new Date()) &&
+               appointmentsInDay.length < 10,
          };
       });
+
+      console.log(availability);
 
       return availability;
    }
