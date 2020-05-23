0a01fa2bab2513d7a289fd88a2671efd3166d6c5
leandrosouzaa <leandror.dev@gmail.com>
2020-05-23 10:37:48 -0300

    dashboard screen

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
+import { FiPower } from 'react-icons/fi';
+
+import { Container, Header, HeaderContent, Profile } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+
 const Dashboard: React.FC = () => {
-   return <h1>Dashboard</h1>;
+   return (
+      <Container>
+         <Header>
+            <HeaderContent>
+               <img src={logoImg} alt="GoBarber" />
+
+               <Profile>
+                  <img
+                     src="https://avatars0.githubusercontent.com/u/51727533?s=460&u=db66dcd045f5e01193f7e162c4bbc193886ae348&v=4"
+                     alt="Leandro Ribeiro"
+                  />
+                  <div>
+                     <span>Bem Vindo</span>
+                     <strong>Leandro Ribeiro</strong>
+                  </div>
+               </Profile>
+
+               <button type="button">
+                  <FiPower />
+               </button>
+            </HeaderContent>
+         </Header>
+      </Container>
+   );
 };
 
 export default Dashboard;
