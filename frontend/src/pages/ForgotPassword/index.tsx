e36a9fe641b509363df61969bfd54839fa25ea85
leandrosouzaa <leandror.dev@gmail.com>
2020-05-25 16:53:26 -0300

    fix in signin link

--- frontend/src/pages/ForgotPassword/index.tsx
+++ frontend/src/pages/ForgotPassword/index.tsx
@@ -87,7 +87,7 @@ const ForgotPassword: React.FC = () => {
                   </Button>
                </Form>
 
-               <Link to="/signin">
+               <Link to="/">
                   <FiLogIn />
                   Voltar ao logon
                </Link>
