53a9c0c20b50f8e52d4401b7b48644cb7cd56c6a
leandrosouzaa <leandror.dev@gmail.com>
2020-05-25 13:25:57 -0300

    update avatar

--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, ChangeEvent } from 'react';
 
 import { FiMail, FiLock, FiUser, FiCamera, FiArrowLeft } from 'react-icons/fi';
 import { Form } from '@unform/web';
@@ -21,7 +21,7 @@ interface ProfileFormData {
 
 const Profile: React.FC = () => {
    const { addToast } = useToast();
-   const { user } = useAuth();
+   const { user, updateUser } = useAuth();
    const history = useHistory();
 
    const formRef = useRef<FormHandles>(null);
@@ -71,6 +71,26 @@ const Profile: React.FC = () => {
       [addToast, history],
    );
 
+   const handleAvatarChange = useCallback(
+      (e: ChangeEvent<HTMLInputElement>) => {
+         if (e.target.files) {
+            const data = new FormData();
+
+            data.append('avatar', e.target.files[0]);
+
+            api.patch('/users/avatar', data).then((response) => {
+               updateUser(response.data);
+
+               addToast({
+                  type: 'success',
+                  title: 'Avatar Atualizado',
+               });
+            });
+         }
+      },
+      [addToast, updateUser],
+   );
+
    return (
       <Container>
          <header>
@@ -88,9 +108,15 @@ const Profile: React.FC = () => {
             >
                <AvatarInput>
                   <img src={user.avatar_url} alt={user.name} />
-                  <button type="button">
+                  <label htmlFor="avatar">
                      <FiCamera />
-                  </button>
+
+                     <input
+                        type="file"
+                        id="avatar"
+                        onChange={handleAvatarChange}
+                     />
+                  </label>
                </AvatarInput>
                <h1>Meu Perfil</h1>
                <Input icon={FiUser} name="name" placeholder="Nome" />
--- frontend/src/pages/Profile/styles.ts
+++ frontend/src/pages/Profile/styles.ts
@@ -78,7 +78,7 @@ export const AvatarInput = styled.div`
       border-radius: 50%;
    }
 
-   button {
+   label {
       position: absolute;
       width: 48px;
       height: 48px;
@@ -88,10 +88,15 @@ export const AvatarInput = styled.div`
       bottom: 0;
       border: 0;
       transition: background-color 0.2s;
+      cursor: pointer;
 
       display: flex;
       align-items: center;
       justify-content: center;
+
+      input {
+         display: none;
+      }
 
       svg {
          height: 20;
