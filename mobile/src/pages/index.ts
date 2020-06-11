fb9cbbb16827c8a2066c6e9bde899e4efd9f1b2e
leandrosouzaa <leandror.dev@gmail.com>
2020-06-11 17:38:38 -0300

    add new pages

--- mobile/src/pages/index.ts
+++ mobile/src/pages/index.ts
@@ -1,5 +1,15 @@
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 import Dashboard from './Dashboard';
+import CreateAppointment from './CreateAppointment';
+import Profile from './Profile';
+import AppointmentCreated from './AppointmentCreated';
 
-export { SignIn, SignUp, Dashboard };
+export {
+   SignIn,
+   SignUp,
+   CreateAppointment,
+   AppointmentCreated,
+   Profile,
+   Dashboard,
+};
--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
 
-import { Dashboard } from '../pages';
+import {
+   Dashboard,
+   CreateAppointment,
+   Profile,
+   AppointmentCreated,
+} from '../pages';
 
 const App = createStackNavigator();
 
 const AppRoutes: React.FC = () => (
    <App.Navigator
       screenOptions={{
-         // headerShown: false,
+         headerShown: false,
          cardStyle: { backgroundColor: '#312e38' },
       }}
    >
+      <App.Screen name="Profile" component={Profile} />
+
       <App.Screen name="Dashboard" component={Dashboard} />
+      <App.Screen name="CreateAppointment" component={CreateAppointment} />
+      <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
    </App.Navigator>
 );
 
