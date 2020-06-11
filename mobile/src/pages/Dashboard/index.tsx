b499223fe6f23ef8482d0e5215c8d92fd1a71321
leandrosouzaa <leandror.dev@gmail.com>
2020-06-11 18:20:18 -0300

    header done

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
-import { View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import { useAuth } from '../../hooks/auth';
+import {
+   Container,
+   Header,
+   HeaderTitle,
+   UserName,
+   ProfileButton,
+   UserAvatar,
+} from './styles';
 
 const Dashboard: React.FC = () => {
-   return <View />;
+   const { user } = useAuth();
+
+   const { navigate } = useNavigation();
+
+   const navigateToProfile = useCallback(() => {
+      navigate('Profile');
+   }, [navigate]);
+
+   return (
+      <Container>
+         <StatusBar barStyle="light-content" backgroundColor="#28262e" />
+
+         <Header>
+            <HeaderTitle>
+               Bem Vindo, {'\n'}
+               <UserName>{user.name}</UserName>
+            </HeaderTitle>
+            <ProfileButton onPress={() => navigateToProfile()}>
+               <UserAvatar source={{ uri: user.avatar_url }} />
+            </ProfileButton>
+         </Header>
+      </Container>
+   );
 };
 
 export default Dashboard;
