0ba230077c9dec5ed3a07fdd3146f0d2c9d831b5
leandrosouzaa <leandror.dev@gmail.com>
2020-06-11 19:08:12 -0300

    provider template

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -3,6 +3,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { StatusBar, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
+import Icon from 'react-native-vector-icons/Feather';
 import api from '../../services/api';
 import { useAuth } from '../../hooks/auth';
 
@@ -13,6 +14,13 @@ import {
    UserName,
    ProfileButton,
    UserAvatar,
+   ProviderContainer,
+   ProviderAvatar,
+   ProviderInfo,
+   ProviderName,
+   ProviderMeta,
+   ProviderMetaText,
+   ProvidersListTitle,
 } from './styles';
 
 export interface ProviderProps {
@@ -30,6 +38,13 @@ const Dashboard: React.FC = () => {
    const navigateToProfile = useCallback(() => {
       navigate('Profile');
    }, [navigate]);
+
+   const navigateToCreateAppointment = useCallback(
+      (providerId: string) => {
+         navigate('CreateAppointment', { providerId });
+      },
+      [navigate],
+   );
 
    useEffect(() => {
       api.get('providers').then((res) => {
@@ -51,9 +66,32 @@ const Dashboard: React.FC = () => {
             </ProfileButton>
          </Header>
          <FlatList
+            style={{ paddingTop: 32, paddingHorizontal: 24, paddingBottom: 16 }}
             data={providers}
             keyExtractor={(provider) => provider.id}
-            renderItem={({ item }) => <UserName>{item.name}</UserName>}
+            ListHeaderComponent={
+               <ProvidersListTitle>Cabeleireiros</ProvidersListTitle>
+            }
+            renderItem={({ item: provider }) => (
+               <ProviderContainer
+                  onPress={() => {
+                     navigateToCreateAppointment(provider.id);
+                  }}
+               >
+                  <ProviderAvatar source={{ uri: provider.avatar_url }} />
+                  <ProviderInfo>
+                     <ProviderName>{provider.name}</ProviderName>
+                     <ProviderMeta>
+                        <Icon name="calendar" size={14} color="#ff9000" />
+                        <ProviderMetaText>Segunda à sexta</ProviderMetaText>
+                     </ProviderMeta>
+                     <ProviderMeta>
+                        <Icon name="clock" size={14} color="#ff9000" />
+                        <ProviderMetaText>8h às 18h</ProviderMetaText>
+                     </ProviderMeta>
+                  </ProviderInfo>
+               </ProviderContainer>
+            )}
          />
       </Container>
    );
--- mobile/src/pages/Dashboard/styles.ts
+++ mobile/src/pages/Dashboard/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
    flex: 1;
@@ -31,4 +32,49 @@ export const UserAvatar = styled.Image`
    width: 56px;
    height: 56px;
    border-radius: 28px;
+`;
+
+export const ProviderContainer = styled(RectButton)`
+   background: #3e3b47;
+   border-radius: 10px;
+   padding: 20px;
+   margin-bottom: 16px;
+   flex-direction: row;
+   align-items: center;
+`;
+
+export const ProvidersListTitle = styled.Text`
+   font-size: 24px;
+   margin-bottom: 24px;
+   color: #f4edef;
+   font-family: 'RobotoSlab-Medium';
+`;
+
+export const ProviderAvatar = styled.Image`
+   width: 72px;
+   height: 72px;
+   border-radius: 36px;
+`;
+
+export const ProviderInfo = styled.View`
+   flex: 1;
+   margin-left: 20px;
+`;
+
+export const ProviderName = styled.Text`
+   font-family: 'RobotoSlab-Medium';
+   font-size: 18px;
+   color: #f4ede8;
+`;
+
+export const ProviderMeta = styled.View`
+   flex-direction: row;
+   align-items: center;
+   margin-top: 8px;
+`;
+
+export const ProviderMetaText = styled.Text`
+   margin-left: 8px;
+   color: #999591;
+   font-family: 'RobotoSlab-Regular';
 `;
