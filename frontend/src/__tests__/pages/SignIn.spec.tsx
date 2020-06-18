d9ceb564684f1220063d95b51db83c04094c0b59
leandrosouzaa <leandror.dev@gmail.com>
2020-06-18 18:53:53 -0300

    init SignIn tests

--- frontend/src/__tests__/pages/SignIn.spec.tsx
+++ frontend/src/__tests__/pages/SignIn.spec.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import SignIn from '../../pages/SignIn';
+
+const mockedHistoryPush = jest.fn();
 
 jest.mock('react-router-dom', () => {
    return {
-      useHistory: jest.fn(),
+      useHistory: () => ({ push: mockedHistoryPush }),
       Link: ({ children }: { children: React.ReactNode }) => children,
    };
 });
 
 describe('SignIn Page', () => {
    it('shoud be able to sign in', () => {
-      const { debug } = render(<SignIn />);
+      const { getByPlaceholderText, getByText } = render(<SignIn />);
 
-      debug();
+      const emailField = getByPlaceholderText('E-mail');
+      const passwordField = getByPlaceholderText('Sua senha Secreta');
+      const buttonElement = getByText('Entrar');
+
+      fireEvent.change(emailField, {
+         target: { value: 'johndoe@example.com' },
+      });
+      fireEvent.change(passwordField, {
+         target: { value: 'secretpassword' },
+      });
+
+      fireEvent.click(buttonElement);
+
+      expect(mockedHistoryPush).toHaveBeenCalledWith('/Dashboard');
    });
 });
